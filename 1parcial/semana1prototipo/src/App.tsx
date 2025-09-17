009f01d6bf5f47863fef8e6fc8efa72914eb6a8a
Leoh <leoanthonyholguinchavez@gmail.com>
2025-09-17 09:29:15 -0500

    agregar base para repositroio para todo el semestre

---
+++ 1parcial/semana1prototipo/src/App.tsx
@@ -0,0 +1,18 @@
+import ArquitectoList from './components/ArquitectoList'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='menu-superior'>
+        <p>Contactos</p>
+        <p>Chats</p>
+        <p>Arquitecto</p>
+
+      </div>
+      <ArquitectoList />
+    </>
+  )
+}
+
+export default App;
--- semana1prototipo/src/interfaces/arquitecto.tsx
+++
@@ -1,6 +0,0 @@
-export interface Arquitecto{
-    id: number;
-    nombre: string;
-    descripcion: string;
-    puntuacion: number;
-}
